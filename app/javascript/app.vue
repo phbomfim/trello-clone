53bcfc32debfbc35d72d665c22ee3372b2aeac0e
phbomfim <paulobommfim@gmail.com>
2021-04-19 14:21:03 -0300

    Update view to vue

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,16 +1,15 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div id="app" class="row">
+    <div v-for="list in original_lists" class="col-3" :key="list.id" :list="list">
+      <h5>{{ list.name }}</h5>
+      <hr />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
+  props: ["original_lists"],
 }
 </script>
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,20 @@ $(document).on("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
 })
+
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+document.addEventListener("turbolinks:load", function() {
+  var element = document.querySelector("#boards")
+  if (element != undefined) {
+    const app = new Vue({
+      el: element,
+      data: {
+        lists: JSON.parse(element.dataset.lists)
+      },
+      template: "<App :original_lists='lists' />",
+      components: { App }
+    })
+  }
+})
