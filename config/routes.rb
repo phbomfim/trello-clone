7d89246849096d0ba4cebc000da6ab75b5da859c
phbomfim <paulobommfim@gmail.com>
2021-04-19 12:41:14 -0300

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,20 @@ authenticate :user, lambda { |u| u.admin? } do
   mount Sidekiq::Web => '/sidekiq'
 
   namespace :madmin do
+    resources :services
+    namespace :active_storage do
+      resources :blobs
+    end
+    namespace :active_storage do
+      resources :attachments
+    end
+    namespace :active_storage do
+      resources :variant_records
+    end
+    resources :users
+    resources :announcements
+    resources :notifications
+    root to: "dashboard#show"
   end
 end
 
